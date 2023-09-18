00cbc080e80e8a85c06d3db580eace4297eadd88
Elpluribusunum <iyogunclarkson@gmail.com>
2023-09-17 20:23:46 -0700

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+0x0F. Python - Object-relational mapping
+I gained knowledge about the application of object-relational mapping to database scripting through this assignment. I learned how to query, create, modify, and delete tables in MySQL using MySQLdb and SQLAlchemy.
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Contains Base, an instance of declarative_base(), and the State class
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    class with each state's id and name as attributes
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
